3b0480dffa71e6b1ec59b9e62b7b22c6c28a9e84
Ajay01103 <125455606+Ajay01103@users.noreply.github.com>
2024-03-15 18:32:51 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "appwrite.io",
-        port: 3000,
-        pathname: "/**",
-      },
-    ],
+    domains: ["appwrite.io"],
   },
-  output: "standalone",
   eslint: {
     ignoreDuringBuilds: true,
   },
 }
 
 export default nextConfig
-
